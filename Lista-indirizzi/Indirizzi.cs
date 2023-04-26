2c856706fc5a1c45185177369d5cdffe8e460abc
emamingio22 <emawankenoby02@gmail.com>
2023-04-26 15:22:57 +0200

    Indirizzo

--- Lista-indirizzi/Indirizzi.cs
+++ Lista-indirizzi/Indirizzi.cs
@@ -8,5 +8,22 @@ namespace Lista_indirizzi
 {
     internal class Indirizzi
     {
+
+        private string nome;
+        private string cognome;
+        private string via;
+        private string città;
+        private string provincia;
+        private int cap;
+
+        public Indirizzo(string nome, string cognome, string via, string città, string provincia, int cap)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.via = via;
+            this.città = città;
+            this.provincia = provincia;
+            this.cap = cap;
+        }
     }
 }
