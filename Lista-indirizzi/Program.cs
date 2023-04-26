d35e8bfeaae647e9e257f243117e5a9cf268e2cf
emamingio22 <emawankenoby02@gmail.com>
2023-04-26 16:56:24 +0200

    finito esercizio

--- Lista-indirizzi/Program.cs
+++ Lista-indirizzi/Program.cs
@@ -1,5 +1,35 @@
 ﻿using Lista_indirizzi;
+using System.IO;
+using System.Xml.Linq;
 
-string path = "C:\\Users\\HP\\Desktop\\Emanuele\\esercizi\\Lista-indirizzi\\Lista-indirizzi\\data\\addresses.csv";
+StreamReader indirizzi = File.OpenText("C:\\Users\\HP\\Desktop\\Emanuele\\esercizi\\Lista-indirizzi\\Lista-indirizzi\\data\\addresses.csv");
 
+List<Indirizzi> listaIndirizzi = new List<Indirizzi>();
 
+indirizzi.ReadLine();
+while (!indirizzi.EndOfStream)
+{
+    string riga = indirizzi.ReadLine(); 
+
+    string[] split = riga.Split(",");
+
+    try
+    {
+      
+        string nome = split[0];
+        string cognome = split[1];
+        string via = split[2];
+        string citta = split[3];
+        string provincia = split[4];
+        int Zip = int.Parse(split[5]);
+
+        Indirizzi indirizzo = new Indirizzi(nome, cognome, via, citta, provincia, Zip);
+
+        listaIndirizzi.Add(indirizzo);
+        Console.WriteLine(indirizzo);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
